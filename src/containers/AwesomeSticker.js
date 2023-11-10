9f1680d94e17acdd3036443c98d5c4df860aaeec
anhpt <anhpt@basebs.com>
2023-11-10 16:51:24 +0700

    [Fix]: Compile to publish

--- src/containers/AwesomeSticker.js
+++ src/containers/AwesomeSticker.js
@@ -19,49 +19,52 @@ const AwesomeSticker = ({ onSelect }) => {
   const [togglePopover, settogglePopover] = React.useState(false);
 
   return (
-    <div className={stylex(styles.root)}>
+    // <div className={stylex(styles.root)}>
+    <div
+      className={`awesomeSticker__popoverContainer ${
+        togglePopover ? "open" : "close"
+      }`}
+      style={{ width: 32, height: 32 }}
+    >
       <div
-        className={`awesomeSticker__popoverContainer ${
-          togglePopover ? "open" : "close"
-        }`}
+        className="awesomeSticker__popoverContainer__button"
         style={{ width: 32, height: 32 }}
+        onClick={() => settogglePopover(!togglePopover)}
       >
-        <div
-          className="awesomeSticker__popoverContainer__button"
+        <StickerIcon
+          className="awesomeSticker__popoverContainer__button__icon"
           style={{ width: 32, height: 32 }}
-          onClick={() => settogglePopover(!togglePopover)}
+        />
+      </div>
+      <OutsideAlerter onClickOutside={() => settogglePopover(false)}>
+        <div
+          className="awesomeSticker__popoverContainer__popover"
+          style={{ position: "absolute", right: 0, bottom: 42, width: 400 }}
         >
-          <StickerIcon className='awesomeSticker__popoverContainer__button__icon' style={{ width: 32, height: 32 }} />
-        </div>
-        <OutsideAlerter onClickOutside={() => settogglePopover(false)}>
-          <div
-            className="awesomeSticker__popoverContainer__popover"
-            style={{ position: "absolute", right: 0, bottom: 42, width: 350 }}
-          >
-            <div className={stylex(styles.stickerCard)}>
-              <div className={stylex(styles.title)}>Quby</div>
-              <div className={stylex(styles.stickerList)}>
-                {stickers?.map((item, index) => {
-                  return (
-                    <Sticker
-                      key={`sticker__${index}`}
-                      frameCount={item?.frameCount}
-                      frameRate={item?.frameRate}
-                      framesPerCol={item?.framesPerCol}
-                      framesPerRow={item?.framesPerRow}
-                      spriteImg={item?.spriteImg}
-                      onSelect={() => {
-                        onSelect && onSelect(item?.id)
-                      }}
-                    />
-                  );
-                })}
-              </div>
+          <div className={stylex(styles.stickerCard)}>
+            <div className={stylex(styles.title)}>Quby</div>
+            <div className={stylex(styles.stickerList)}>
+              {stickers?.map((item, index) => {
+                return (
+                  <Sticker
+                    key={`sticker__${index}`}
+                    frameCount={item?.frameCount}
+                    frameRate={item?.frameRate}
+                    framesPerCol={item?.framesPerCol}
+                    framesPerRow={item?.framesPerRow}
+                    spriteImg={item?.spriteImg}
+                    onSelect={() => {
+                      onSelect && onSelect(item?.id);
+                    }}
+                  />
+                );
+              })}
             </div>
           </div>
-        </OutsideAlerter>
-      </div>
+        </div>
+      </OutsideAlerter>
     </div>
+    // </div>
   );
 };
 
@@ -69,9 +72,9 @@ export default AwesomeSticker;
 
 const styles = stylex.create({
   root: {
-   position: 'fixed',
-   bottom: 20,
-   right: 20,
+     position: 'fixed',
+     bottom: 20,
+     right: 20,
   },
   stickerCard: {
     display: "block",
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,3 @@
+import './style.css';
 export { default as AwesomeSticker } from './AwesomeSticker';
 export { default as AwsSticker } from './AwsSticker';
